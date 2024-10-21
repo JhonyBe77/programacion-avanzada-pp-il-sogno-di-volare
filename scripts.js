d831cee8ec046fb83d72b4b01163ce9ce841507d
JhonyBe77 <jjruiperezf@gmail.com>
2024-10-21 08:40:21 +0200

    add/scripts

--- scripts.js
+++ scripts.js
@@ -1,63 +1,95 @@
 const civilAircrafts = [
-    'assets/avion-civil-1.jpg',
-    'assets/avion-civil-2.jpg',
-    'assets/avion-civil-3.jpg',
-    'assets/avion-civil-4.jpg',
-    'assets/avion-civil-5.jpg'
-];
+    'avion-civil-1.jpg',
+    'avion-civil-2.jpg',
+    'avion-civil-3.jpg',
+    'avion-civil-4.jpg',
+    'avion-civil-5.jpg'
+].map(image => `assets/${image}`);
 
 const militaryAircrafts = [
-    'assets/avion-militar-1.jpg',
-    'assets/avion-militar-2.jpg',
-    'assets/avion-militar-3.jpg',
-    'assets/avion-militar-4.jpg',
-    'assets/avion-militar-5.jpg'
-];
+    'avion-militar-1.jpg',
+    'avion-militar-2.jpg',
+    'avion-militar-3.jpg',
+    'avion-militar-4.jpg',
+    'avion-militar-5.jpg'
+].map(image => `assets/${image}`);
 
 const civilHelicopters = [
-    'assets/helicoptero-civil-1.jpg',
-    'assets/helicoptero-civil-2.jpg',
-    'assets/helicoptero-civil-3.jpg',
-    'assets/helicoptero-civil-4.jpg',
-    'assets/helicoptero-civil-5.jpg'
-];
+    'helicoptero-civil-1.jpg',
+    'helicoptero-civil-2.jpg',
+    'helicoptero-civil-3.jpg',
+    'helicoptero-civil-4.jpg',
+    'helicoptero-civil-5.jpg'
+].map(image => `assets/${image}`);
 
 const militaryHelicopter = [
-    'assets/helicoptero-militar-1.jpg',
-    'assets/helicoptero-militar-2.jpg',
-    'assets/helicoptero-militar-3.jpg',
-    'assets/helicoptero-militar-4.jpg',
-    'assets/helicoptero-militar-5.jpg'
-];
+    'helicoptero-militar-1.jpg',
+    'helicoptero-militar-2.jpg',
+    'helicoptero-militar-3.jpg',
+    'helicoptero-militar-4.jpg',
+    'helicoptero-militar-5.jpg'
+].map(image => `assets/${image}`);
 
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages;
+        this.militaryImages = militaryImages;
     }
-    
+
     getRandomCivil() {
+        const randomIndex = Math.floor(Math.random() * this.civilImages.length);
+        return this.civilImages[randomIndex];
     }
-    
+
     getRandomMilitary() {
+        const randomIndex = Math.floor(Math.random() * this.militaryImages.length);
+        return this.militaryImages[randomIndex];
     }
-    
+
     getAll() {
+        return [...this.civilImages, ...this.militaryImages];
     }
 }
 
 class Painter {
     constructor() {
+        this.galleryContainer = document.createElement('div');
+        this.galleryContainer.id = 'gallery';
+        document.body.appendChild(this.galleryContainer);
     }
 
-    createGallery() {
+    createGallery(images) {
+        this.clearGallery();
+        images.forEach(imageUrl => {
+            const imgTag = this.createImageTag(imageUrl);
+            this.galleryContainer.appendChild(imgTag);
+        });
     }
 
     createImageTag(imageUrl) {
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        img.alt = 'Image not found';
+        img.classList.add('gallery-image');
+        return img;
     }
 
     paintSingleImage(imageUrl) {
+        this.clearGallery();
+        const imgTag = this.createImageTag(imageUrl);
+        this.galleryContainer.appendChild(imgTag);
     }
 
     paintMultipleImages(arrayOfImages) {
+        this.clearGallery();
+        arrayOfImages.forEach(imageUrl => {
+            const imgTag = this.createImageTag(imageUrl);
+            this.galleryContainer.appendChild(imgTag);
+        });
+    }
+
+    clearGallery() {
+        this.galleryContainer.innerHTML = '';
     }
 }
 
